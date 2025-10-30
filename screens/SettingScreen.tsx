38c9929de2b32d93b2dcb58effbd63accdc847b5
Julio Cesar <110637361+julioo19@users.noreply.github.com>
2025-10-30 05:00:16 +0000

    Se cambio el Prop a CompositeNavigationProp para poder navegar por varias pantallas

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -1,18 +1,37 @@
 import { Button, ScrollView, Settings, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import CustomButton from '../components/CustomButton'
-import { useNavigation } from '@react-navigation/native'
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import SettingNavButton from '../components/SettingNavButton'
-
+import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 type SettingStackParamList = {
     SettingScreen: undefined
     AboutScreen: undefined
-    AuthScreen: undefined
+    HomeScreen: undefined
 }
 
-type SettingStackNavigationProp = NativeStackNavigationProp<SettingStackParamList, 'SettingScreen'>;
+type TabParamList = {
+  Home: undefined;
+  Carrito: undefined;
+  Favoritos: undefined;
+  Ajustes: undefined;
+};
+
+// 3. Define el RootStack
+type RootStackParamList = {
+  MainApp: undefined;
+  NotificationScreen: undefined;
+  AuthScreen: undefined;
+};
+
+type SettingStackNavigationProp = CompositeNavigationProp<
+BottomTabNavigationProp<TabParamList>,
+NativeStackNavigationProp<RootStackParamList>>;
 
 const SettingScreen = () => {
     const navigation = useNavigation<SettingStackNavigationProp>();
@@ -24,27 +43,28 @@ const SettingScreen = () => {
         */
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
-                <ScrollView>
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={90} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
-                    <SettingNavButton onPress={() => console.log("asa")} title='Auth' unstable_pressDelay={70} />
+                <ScrollView style={styles.box}>
+                    <SettingNavButton
+                        onPress={() => navigation.navigate('Home')}
+                        title='Inicio'
+                        unstable_pressDelay={90}
+                        Icon={<Ionicons name="home-outline" size={20} color="black" />} />
+                    <SettingNavButton
+                        onPress={() => console.log("Acerca de Grone Coffee")}
+                        title='Acerca de Grone Coffee'
+                        unstable_pressDelay={90}
+                        Icon={<Feather name="info" size={20} color="black" />} />
+                    <SettingNavButton
+                        onPress={() => console.log("Tema Osuro o Claro")}
+                        title='Temas'
+                        unstable_pressDelay={90}
+                        Icon={<MaterialCommunityIcons name="theme-light-dark" size={20} color="black" />} />
+
+                    <SettingNavButton
+                        onPress={() => console.log("Notificaciones")}
+                        title='Notificaciones'
+                        unstable_pressDelay={90}
+                        Icon={<AntDesign name="bell" size={20} color="black" />} />
 
                 </ScrollView>
             </SafeAreaView>
@@ -57,6 +77,7 @@ export default SettingScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        //backgroundColor: 'rgb(0,0,0)',
     },
 
     box: {
